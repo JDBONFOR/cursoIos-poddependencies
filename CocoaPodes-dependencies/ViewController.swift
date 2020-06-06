8beada3b607d661061001d01e6d3a9bda1d245bd
Juan Bonforti <juan.bonforti@gmail.com>
2020-06-06 11:10:44 -0300

    Initial Commit

---
+++ CocoaPodes-dependencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoaPodes-dependencies
+//
+//  Created by Juan Bonforti on 06/06/2020.
+//  Copyright © 2020 Juan Bonforti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
